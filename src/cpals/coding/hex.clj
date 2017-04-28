ec28524fd30f89c377e4258ad06c198b02a17afc
Stig Brautaset <stig@brautaset.org>
2017-04-28 22:54:11 +0100

    Decode hex

--- src/cpals/coding/hex.clj
+++ src/cpals/coding/hex.clj
@@ -6,3 +6,22 @@
   (->> bytes
        (map #(format "%02X" %))
        (apply str)))
+
+(def ^:private alphabet "0123456789abcdef")
+
+(def ^:private hex-to-int
+  (merge
+   (zipmap alphabet (iterate inc 0))
+   (zipmap (.toUpperCase alphabet) (iterate inc 0))))
+
+(defn- hex-combine
+  [hi lo]
+  (bit-or (bit-shift-left hi 4) lo))
+
+(defn decode
+  "Decode a hex string to a sequence of bytes"
+  [hex]
+  (byte-array (->> hex
+                   (map hex-to-int)
+                   (partition-all 2)
+                   (map #(apply hex-combine %)))))
--- test/cpals/coding/hex_test.clj
+++ test/cpals/coding/hex_test.clj
@@ -1,5 +1,9 @@
 (ns cpals.coding.hex-test
   (:require [clojure.test :as t]
+            [clojure.test.check
+             [clojure-test :refer [defspec]]
+             [generators :as gen]
+             [properties :as prop]]
             [cpals.coding.hex :refer :all]))
 
 (t/deftest encode-test
@@ -10,3 +14,19 @@
       [254 1 15] "FE010F"))
   (t/testing "byte arrays"
     (t/is (= "FE010F" (encode (byte-array [254 1 15]))))))
+
+(t/deftest decode-test
+  (t/are [in out]
+      (java.util.Arrays/equals (byte-array out)
+                               (decode in))
+    "00" [0]
+    "FF" [255]
+    "FE010F" [254 1 15]
+    "fe010f" [254 1 15]))
+
+
+(defspec roundtrip-test
+  100
+  (prop/for-all [v (gen/not-empty gen/bytes)]
+                (let [encoded (encode v)]
+                  (= encoded (encode (decode encoded))))))
