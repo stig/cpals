28c3cf6ab5a1fb21775c2fae959b512a0ca75191
Stig Brautaset <stig@brautaset.org>
2017-04-28 21:54:50 +0100

    inital commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cpals
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cpals/core_test.clj
@@ -0,0 +1,7 @@
+(ns cpals.core-test
+  (:require [clojure.test :refer :all]
+            [cpals.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
