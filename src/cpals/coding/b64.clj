fec5fbc82096900f9b20f94d806f4e0d8e3c74d9
Stig Brautaset <stig@brautaset.org>
2017-04-29 15:14:13 +0100

    Base 64 encode/decode

---
+++ src/cpals/coding/b64.clj
@@ -0,0 +1,67 @@
+(ns cpals.coding.b64)
+
+
+(def ^:private alphabet
+  "The alphabet of characters for Base64 encoding."
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+
+(defn- b64-combine-bytes
+  "1-3 bytes that will be combined into an int"
+  [xs]
+  (loop [xs (take 3 (concat xs (repeat 0)))
+         acc 0]
+    (if (seq xs)
+      (recur (rest xs)
+             (+ (bit-shift-left acc 8)
+                (bit-and 0xff (first xs))))
+      acc)))
+
+(defn- b64-encode-triplet
+  [xs]
+  (let [combined (b64-combine-bytes xs)
+        want-bytes (inc (count xs))
+        want-bits (take want-bytes [18 12 6 0])
+        quad (for [i want-bits]
+               (.charAt alphabet (bit-and 2r111111 (bit-shift-right combined i))))]
+    (take 4 (concat quad [\= \=]))))
+
+(defn encode
+  "Encode a sequence of bytes to a base64-encoded string"
+  [bytes]
+  (->> bytes
+       (partition-all 3)
+       (map b64-encode-triplet)
+       flatten
+       (apply str)))
+
+(def ^:private decode-char
+  (conj (zipmap alphabet (iterate inc 0))
+        [\= 0]))
+
+(defn- b64-combine-chars
+  "Quad of chars that will be combined into an int"
+  [xs]
+  (loop [xs xs acc 0]
+    (if (seq xs)
+      (recur (rest xs)
+             (+ (bit-shift-left acc 6)
+                (decode-char (first xs))))
+      acc)))
+
+(defn- b64-decode-quad
+  [quad]
+  ;; Feels very inefficcient; but it'll do, for now.
+  (let [combined (b64-combine-chars quad)
+        want-bytes (dec (count (filter (comp not #{\=}) quad)))
+        want-bits (take want-bytes [16 8 0])]
+    (for [i want-bits]
+      (bit-and 0xff (bit-shift-right combined i)))))
+
+(defn decode
+  "Decode a base64-encoded string"
+  [encoded]
+  (->> encoded
+       (partition 4)
+       (map b64-decode-quad)
+       flatten
+       byte-array))
---
+++ test/cpals/coding/b64_test.clj
@@ -0,0 +1,43 @@
+(ns cpals.coding.b64-test
+  (:require [clojure.test :as t]
+            [clojure.test.check
+             [clojure-test :refer [defspec]]
+             [generators :as gen]
+             [properties :as prop]]
+            [cpals.coding.b64 :refer [decode encode]]))
+
+(t/deftest encode-test
+  (t/testing "simple cases"
+    (t/are [x y] (= x (encode (byte-array y)))
+      "AA==" [0]
+      "AAA=" [0 0]
+      "AAAA" [0 0 0]))
+
+  (t/testing "en.wikipedia.org/wiki/Base64#Padding"
+    (t/are [x y] (= (encode (.getBytes x "UTF-8")) y)
+      "pleasure." "cGxlYXN1cmUu"
+      "leasure." "bGVhc3VyZS4="
+      "easure." "ZWFzdXJlLg=="
+      "asure." "YXN1cmUu"
+      "sure." "c3VyZS4=")))
+
+(t/deftest decode-test
+  (t/testing "simplest cases"
+    (t/are [x y] (= x (vec (decode y)))
+      [0] "AA=="
+      [0 0] "AAA="
+      [0 0 0] "AAAA"))
+
+  (t/testing "en.wikipedia.org/wiki/Base64#Padding"
+    (t/are [x y] (= (encode (.getBytes x "UTF-8")) y)
+      "pleasure." "cGxlYXN1cmUu"
+      "leasure." "bGVhc3VyZS4="
+      "easure." "ZWFzdXJlLg=="
+      "asure." "YXN1cmUu"
+      "sure." "c3VyZS4=")))
+
+(defspec roundtrip-test
+  100
+  (prop/for-all [v (gen/not-empty gen/bytes)]
+                (let [encoded (encode v)]
+                  (= encoded (encode (decode encoded))))))
