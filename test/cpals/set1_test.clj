eef3dc04185fa0f41477ac4e2bc04cd8276aa530
Stig Brautaset <stig@brautaset.org>
2017-04-29 23:23:55 +0100

    Set 1 Challenge 5: XOR Repeating Key Cipher

--- test/cpals/set1_test.clj
+++ test/cpals/set1_test.clj
@@ -46,3 +46,12 @@
              (->> (read-lines "4.txt")
                   (map hex/decode)
                   detect-single-char-xor-cipher)))))
+
+(t/deftest challenge5
+  "http://cryptopals.com/sets/1/challenges/5"
+  (t/testing "xor repeating key cipher"
+    (let [cleartext "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+          key "ICE"
+          ciphertext "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"]
+      (t/is (= (hex/encode (xor-buffer-with-key (map byte cleartext) key))
+               ciphertext)))))
